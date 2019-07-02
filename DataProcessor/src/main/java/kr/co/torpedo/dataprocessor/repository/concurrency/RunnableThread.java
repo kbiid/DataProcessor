984629229cdd392248581cace278113ff591e30d
user <user@DESKTOP-6BBACVK>
2019-07-02 18:30:50 +0900

    thread 구상중

---
+++ DataProcessor/src/main/java/kr/co/torpedo/dataprocessor/repository/concurrency/RunnableThread.java
@@ -0,0 +1,17 @@
+package kr.co.torpedo.dataprocessor.repository.concurrency;
+
+import kr.co.torpedo.dataprocessor.repository.RepositoryHandler;
+
+public class RunnableThread implements Runnable {
+	private RepositoryHandler handler;
+
+	public void setHandler(RepositoryHandler handler) {
+		this.handler = handler;
+	}
+
+	@Override
+	public void run() {
+		handler.insert();
+	}
+
+}
